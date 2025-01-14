c1d709a0b66de4632a5ce58baa2cf59e974e84e2
Aniketikhar <aniketikhar88@gmail.com>
2025-01-14 11:08:40 +0530

    tailwind css config 1

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Hero from "@/Components/Hero";
 import { fetchAllJobs } from "@/Utils/utils";
+import "./globals.css";
 
 export default async function Home() {
   let jobs = await fetchAllJobs();
