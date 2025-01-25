19c3cdd2f4602664306dc992e34b16687839b694
Aniketikhar <aniketikhar88@gmail.com>
2025-01-25 19:44:43 +0530

    hero jobs are from client

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -2,49 +2,56 @@
 import React, { useEffect, useState } from "react";
 import Searchbar from "./Searchbar";
 import JobsSection from "./JobsSection";
-import { fetchCategory } from "@/Utils/utils";
+import { fetchAllJobs, fetchCategory } from "@/Utils/utils";
 import { assets } from "@/Assets/assets";
-import ConsultationSection from "./Cunsultancy";
 import JobsByQualAndLoc from "./JobsByQualAndLoc";
 import Link from "next/link";
 import SocialMediaBar from "./SocialMediaBar";
 import Jobpost from "./Jobpost";
 
-
-
-const Hero = ({ jobs }) => {
+const Hero = () => {
   const [categories, setCategories] = useState();
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [filteredJobs, setFilteredJobs] = useState(jobs);
+  const [filteredJobs, setFilteredJobs] = useState();
   const [position, setPosition] = useState("");
   const [location, setLocation] = useState("");
+  const [jobs, setJobs] = useState([]);
 
+  useEffect(() => {
+    fetchAllJobs()
+      .then((data) => {
+        setJobs(data);
+        setFilteredJobs(data);
+        console.log("this are jobs comes to hero page: ",data)
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       filterJobs(position, location);
     }, 1000);
-  
+
     return () => clearTimeout(timer);
-
-  },[position, location])
-
+  }, [position, location, jobs]);
 
   const filterJobs = (position, location) => {
     const lowerPosition = position?.toLowerCase() || "";
     const lowerLocation = location?.toLowerCase() || "";
-  
+
     const result = jobs?.filter((job) => {
       const matchesPosition =
         job.title?.toLowerCase().includes(lowerPosition) ||
         job.jobRole?.toLowerCase().includes(lowerPosition) ||
         job.company?.toLowerCase().includes(lowerPosition);
-  
-      const matchesLocation = job.location?.toLowerCase().includes(lowerLocation);
-  
+
+      const matchesLocation = job.location
+        ?.toLowerCase()
+        .includes(lowerLocation);
+
       return matchesPosition && matchesLocation;
     });
-  
+
     setFilteredJobs(result);
   };
 
@@ -59,17 +66,16 @@ const Hero = ({ jobs }) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % backgroundImages?.length);
+      setCurrentSlide(
+        (prevSlide) => (prevSlide + 1) % backgroundImages?.length
+      );
     }, 5000);
 
     return () => clearInterval(interval); // Cleanup interval on unmount
   }, [backgroundImages?.length]);
 
-  
-
   return (
     <>
-    
       <section id="hero" className=" text-center md:text-left">
         <div
           className=" py-10"
@@ -94,17 +100,23 @@ const Hero = ({ jobs }) => {
             </p>
           </div>
           <div className="py-10 container mx-auto">
-            <Searchbar filterJobs={filterJobs} position={position} setPosition={setPosition} location={location} setLocation={setLocation} />
+            <Searchbar
+              filterJobs={filterJobs}
+              position={position}
+              setPosition={setPosition}
+              location={location}
+              setLocation={setLocation}
+            />
           </div>
           <div className="container mx-auto pb-10 flex flex-wrap gap-2 md:gap-4 flex-shrink-0">
             {categories?.map((category, index) => (
               <Link
-              href={'/categories/category/' + category._id}
-              key={index}
-              className="px-3 py-1 border-2 text-white border-gray-100 rounded-sm hover:border-blue-600 hover:bg-blue-600 hover:text-white"
-            >
-              {category.name}
-            </Link>
+                href={"/categories/category/" + category._id}
+                key={index}
+                className="px-3 py-1 border-2 text-white border-gray-100 rounded-sm hover:border-blue-600 hover:bg-blue-600 hover:text-white"
+              >
+                {category.name}
+              </Link>
             ))}
           </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -1,17 +1,14 @@
 import Hero from "@/Components/Hero";
-import { fetchAllJobs } from "@/Utils/utils";
 import "./globals.css";
 import WrapPopUp from "@/Components/WrapPopUp";
-import Loading from "@/Components/Loading";
 
 export default async function Home() {
-  let jobs = await fetchAllJobs();
 
   return (
     <>
       <WrapPopUp />
 
-      <Hero jobs={jobs} />
+      <Hero />
     </>
   );
 }
